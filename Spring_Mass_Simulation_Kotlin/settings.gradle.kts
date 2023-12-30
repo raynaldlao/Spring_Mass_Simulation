c979e7de64251c69a4d67b72e8da0c311c7dfe90
raynaldlao <genelee1825@gmail.com>
2023-12-30 01:27:33 +0100

    Initial commit

---
+++ Spring_Mass_Simulation_Kotlin/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Spring_Mass_Simulation_Kotlin"
+include(":app")
+ 
